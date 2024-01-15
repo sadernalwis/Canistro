3c7383530dd4545cf1b86828028c4a4b2a0ff996
sadernalwis <sadernalwis@outlook.com>
2024-01-15 23:54:44 +0530

    Canistro

--- Medusa/Mirror/Terminal/Canistro/Canistro.js
+++ Medusa/Mirror/Terminal/Canistro/Canistro.js
@@ -1,6 +1,66 @@
 import { Block } from "Medusa/Mirror/Terminal/Block/Block.js"
 import { HTML } from "Medusa/Parseltongue/HTML/HTML.js";
+import { SVG } from "Medusa/Parseltongue/SVG/SVG.js";
 
+
+export class Canister {
+	geometry(radius){
+		const stroke = radius * 0.2;
+		const fit_rad = radius - stroke * 2;
+		const circumference = fit_rad * 2 * Math.PI;
+		return [stroke, fit_rad, circumference]
+	}
+	attributes(radius=100, x=0, y=0){
+		const [stroke, fit_rad, circumference] = this.geometry(radius)
+		return {
+			r: fit_rad, cx: x, cy: y,
+			style: `stroke-dashoffset:${circumference}`,
+			strokeDasharray: `${circumference} ${circumference}`,
+			strokeWidth: stroke,
+			fill: "transparent", stroke: '#e74c3c', }}
+
+	style(radius){
+		const [stroke, fit_rad, circumference] = this.geometry(radius)
+		return {
+			strokeWidth     : 10,
+			strokeLinecap   : 'round',
+			fill            : 'lightgrey',
+			strokeDashoffset: 0,
+			strokeDasharray : circumference,
+			strokeLinecap   : 'round',
+			// transformOrigin : `${radius}px ${radius}px`,
+			transform       : `rotate(-90deg)`,
+			animation       : `dash 2s ease-in-out infinite`,
+			transition		: 'all 250ms  ease  0ms' } }
+
+	configure(radius){
+		SVG.configure(circle, this.attributes(radius), true)
+		SVG.configure(this.svg_root, {}, true)
+		SVG.style(circle , this.style(radius)); 
+	}
+
+	canister(radius){
+		const svg_root = this.svg_root
+		const id = `canister-textpath-${this.name}`
+		const path 		= SVG.make('path','',[],{d:"M0, 200a200, 200 0 1, 0 400, 0a200, 200 0 1, 0 -400, 0"}, id)
+		const def 		= SVG.make('def','',[path],{})
+		const textpath 	= SVG.make('textPath','',[],{})
+		const text 		= SVG.make('text','',[textpath],{d}, '', `sample text. sample text. sample text. sample text. sample text. `)
+		const circle 	= SVG.make("circle", "progress-ring", [], {} )
+		const name 		= SVG.make("text",   "", [], { "pointer-events": 'none', fontSize:"18", textAnchor:"middle", dominantBaseline:"middle", fill:"black"}, '', this.name)
+		SVG.put(svg_root, def,  0, true);
+		SVG.put(svg_root, text, 1, true);
+		SVG.put(svg_root, circle, 2, true);
+		SVG.put(svg_root, name, 3, true);
+	}
+
+	constructor(terminal, name){
+		this.terminal = terminal
+		this.id = id
+		this.html = {}
+        this.node = this.canister()
+	}
+}
 export class Canistro {
 
 	render() {
